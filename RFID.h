e80b807e4c0d80173ddeb633d8d2befb69632d4e
CJ <charlesjones9991@gmail.com>
2020-02-18 17:39:08 +0000

    Added RFID module with interrupt

---
+++ RFID.h
@@ -0,0 +1,26 @@
+/* 
+ * File:   RFID.h
+ * Author: charl
+ *
+ * Created on 18 February 2020, 10:54
+ */
+
+#ifndef RFID_H
+#define	RFID_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+void init_RFID(void);
+char getCharSerial(void);
+void processRFID(char latestChar);
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* RFID_H */
+
