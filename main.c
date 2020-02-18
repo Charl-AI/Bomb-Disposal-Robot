ffbe272b956602a862cdb2271157819ceb891e70
CJ <charlesjones9991@gmail.com>
2020-02-18 17:38:38 +0000

    Added RFID module with interrupt

--- main.c
+++ main.c
@@ -16,11 +16,10 @@
 
 #include "LCDIO.h"
 #include "dc_motor.h"
-
+#include "RFID.h"
 
 /*****************************************************************************/
 //Global variables defined here
-
 
 /*****************************************************************************/
 // setup function, initialise registers here
@@ -29,13 +28,25 @@ void setup(void)
     // set the clock to 8 MHz and wait for it to stabilise
     OSCCON = 0x72; 
     while(!OSCCONbits.IOFS);
+    INTCONbits.GIEH = 1; //Global high priority interrupt enable
+    INTCONbits.GIEL = 1;// enable low priority interrupts
+    RCONbits.IPEN=1; // enable interrupt prioritisation
     
-    
+    LCD_Init();
+    init_RFID();
 }
 /*****************************************************************************/
 // High priority interrupt service routine
 void __interrupt(high_priority) InterruptHandlerHigh (void)
 {
+    // Trigger interrupt when a character is read from the RFID
+    if(PIR1bits.RCIF)
+    {
+        char temp = RCREG;
+        processRFID(temp);
+        
+        
+    }
     
 }
 /*****************************************************************************/
